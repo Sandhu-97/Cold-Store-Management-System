de73fcb481c028b6c41b2c1cf0e4eee9606658f7
Daanveer Singh <dhveersandhu@gmail.com>
2024-03-01 02:12:58 +0530

    added login check

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import flet as ft
+import database 
 
 def main(page: ft.Page):
     page.title = 'Cold Store Management System'
     page.window_maximized = True
 
-    # def login_click(e):
+    def login_click(e):
+        password = database.password_verify(phone_number=login_phone_field.value)
+        if password and password==login_password_field.value:
+            page.go('/home')
+        else:
+            print('password not correct', password)
+
+    def signup_click(e):
+        pass
+        
+            
         
 
     login_phone_field = ft.TextField(label='Phone Number')
     login_password_field = ft.TextField(label='Password', password=True, can_reveal_password=True)
-    login_login_button = ft.ElevatedButton("Login", bgcolor=ft.colors.RED_400)
+    login_login_button = ft.ElevatedButton("Login", bgcolor=ft.colors.RED_400, on_click=login_click)
     login_signup_button = ft.ElevatedButton("Sign Up", on_click=lambda _: page.go("/signup"), bgcolor=ft.colors.RED_ACCENT_400)
+
+    signup_name_field = ft.TextField(label='Name')
+    signup_phone_field = ft.TextField(label='Phone Number')
+    signup_password_field = ft.TextField(label='Password', password=True, can_reveal_password=True)
+    signup_signup_button = ft.ElevatedButton("Sign Up", bgcolor=ft.colors.RED_ACCENT_400)
 
         
     def route_change(route):
@@ -38,13 +54,14 @@ def main(page: ft.Page):
                     "/signup",
                     [
                         ft.AppBar(title=ft.Text("Sign Up As A New User"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.TextField(label='Name'),
-                        ft.TextField(label='Phone Number'),
-                        ft.TextField(label='Password', password=True, can_reveal_password=True)
-                        # ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        signup_name_field,
+                        signup_phone_field,
+                        signup_password_field,
+                        signup_signup_button
                     ],
                 )
             )
+        
         page.update()
 
     def view_pop(view):
